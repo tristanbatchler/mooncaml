346787c92af774b933089bf7a46ec4b5ffab71ba
Tristan Batchler <info@tbat.me>
2026-03-08 19:41:52 +1000

    Improve text wrapping on curses client chatbox etc

--- lib/client/drawing.ml
+++ lib/client/drawing.ml
@@ -1,16 +1,52 @@
 let draw_map (state : Types.state) =
   Curses.werase state.ui.map_win;
   Curses.box state.ui.map_win 0 0;
-  ignore (Curses.mvwaddch state.ui.map_win state.player_y state.player_x (Char.code '@'));
+  (* +1 offset so game coord (0,0) maps to window coord (1,1), inside the border *)
+  ignore
+    (Curses.mvwaddch state.ui.map_win (state.player.y + 1) (state.player.x + 1) (Char.code '@'));
   ignore (Curses.wrefresh state.ui.map_win)
+;;
+
+(* Split a message into lines that fit within [width] columns *)
+let wrap_text width msg =
+  let len = String.length msg in
+  if len = 0
+  then [ "" ]
+  else (
+    let rec go acc pos =
+      if pos >= len
+      then List.rev acc
+      else (
+        let chunk_len = min width (len - pos) in
+        go (String.sub msg pos chunk_len :: acc) (pos + chunk_len))
+    in
+    go [] 0)
 ;;
 
 let draw_log (state : Types.state) =
   let w = state.ui.log_win in
+  let _, win_w = Curses.getmaxyx w in
+  let inner_w = max 1 (win_w - 2) in
+  let avail_rows = Windows.log_height - 2 in
   Curses.werase w;
   Curses.box w 0 0;
-  let visible_msgs = Util.take (Windows.log_height - 2) state.log |> List.rev in
-  List.iteri (fun i msg -> ignore (Curses.mvwaddstr w (i + 1) 1 msg)) visible_msgs;
+  (* state.log is newest-first.  Collect wrapped line-groups from newest to
+     oldest, prepending each group so List.concat yields oldest-first order. *)
+  let rec collect acc remaining = function
+    | ([] | _ :: _) when remaining <= 0 -> acc
+    | [] -> acc
+    | msg :: rest ->
+      let lines = wrap_text inner_w msg in
+      let n = List.length lines in
+      if n <= remaining
+      then collect (lines :: acc) (remaining - n) rest
+      else (
+        (* Partially visible: show the last [remaining] lines *)
+        let tail = List.rev lines |> Util.take remaining |> List.rev in
+        tail :: acc)
+  in
+  let visible_lines = collect [] avail_rows state.log |> List.concat in
+  List.iteri (fun i line -> ignore (Curses.mvwaddstr w (i + 1) 1 line)) visible_lines;
   ignore (Curses.wrefresh w)
 ;;
 
